9bd6aee1c2b6872ab665fbdfa33555ac37982b4f
Roberto B <roberto.butti@gmail.com>
2020-10-29 21:55:00 +0100

    Full coverage tests for RoutingV8

--- tests/Here/RestApi/RoutingV8Test.php
+++ tests/Here/RestApi/RoutingV8Test.php
@@ -2,6 +2,10 @@
 
 namespace HiFolks\Milk\Tests\Here\RestApi;
 
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use HiFolks\Milk\Here\RestApi\Common\RestConfig;
 use HiFolks\Milk\Here\RestApi\RoutingV8;
 use PHPUnit\Framework\TestCase;
 
@@ -127,6 +131,141 @@ class RoutingV8Test extends TestCase
         $url = "https://router.hereapi.com/v8/routes?return=polyline%2Cactions%2Cinstructions&origin=52.516,13.3779&destination=52.5185,13.4283&apiKey=xxx";
         $this->assertSame($url, $routing->getUrl(), "Routing: Basic GET routing with apiKey");
 
+
+    }
+
+    public function testBasicRoutingWithMock()
+    {
+        $responseString = '{
+  "routes": [
+    {
+      "id": "1793a897-0843-4957-ab63-c61e0f13aff2",
+      "sections": [
+        {
+          "id": "c3e300ab-a880-4fab-9bb2-702355b2bf6b",
+          "type": "vehicle",
+          "actions": [
+            {
+              "action": "depart",
+              "duration": 126,
+              "instruction": "Head toward Chausseestraße on Invalidenstraße. Go for 1.2 km.",
+              "offset": 0
+            },
+            {
+              "action": "arrive",
+              "duration": 0,
+              "instruction": "Arrive at Invalidenstraße.",
+              "offset": 78
+            }
+          ],
+          "departure": {
+            "time": "2019-12-05T15:15:56+01:00",
+            "place": {
+              "type": "place",
+              "location": {
+                "lat": 52.53100287169218,
+                "lng": 13.38464098982513
+              }
+            }
+          },
+          "arrival": {
+            "time": "2019-12-05T15:18:02+01:00",
+            "place": {
+              "type": "place",
+              "location": {
+                "lat": 52.52639072947204,
+                "lng": 13.368653766810894
+              }
+            }
+          },
+          "summary": {
+            "duration": 126,
+            "length": 1200
+          },
+          "polyline": "BG2znmkDi89wZ9ChKAA1IvfAArH5cAArHvbAA1CrJAArF5SAAtP9yBAAT1E3E3QAA_BrH3M9sBAA_F5SAA3KlkBAA1EtNAApB_DAAhC1EAApB1I_D5OAA3ErPAApFtTAAtN_wBAA1GtVAA5U3lCAA_DhOAA3KliBAAtXjvCAArDtLAA1EhQAA1CrJAA_BrFAAvbl9CAAhIvZ_FtTrDtLAAV1I1CtNAA1E3QAArLnoB1G5YAAhGhSpBrFAAhC1GAA1FxT",
+          "spans": [
+            {
+              "offset": 0,
+              "names": [
+                {
+                  "value": "Invalidenstraße",
+                  "language": "de"
+                }
+              ],
+              "length": 787
+            },
+            {
+              "offset": 49,
+              "names": [
+                {
+                  "value": "Invalidenstraße",
+                  "language": "de"
+                },
+                {
+                  "value": "Sandkrugbrücke",
+                  "language": "de"
+                }
+              ],
+              "length": 51
+            },
+            {
+              "offset": 57,
+              "names": [
+                {
+                  "value": "Invalidenstraße",
+                  "language": "de"
+                }
+              ],
+              "length": 362
+            }
+          ],
+          "transport": {
+            "mode": "car"
+          }
+        }
+      ]
+    }
+  ]
+}';
+        $responseString400 = '{
+  "title": "Malformed request",
+  "status": 400,
+  "code": "E605001",
+  "cause": "missing field `destination`",
+  "action": "",
+  "correlationId": "4199533b-6290-41db-8d79-edf4f4019a74"
+}';
+        $responseString401 = '{
+  "error": "Unauthorized",
+  "error_description": "No credentials found"
+}';
+        $mock = new MockHandler([
+            new Response(200, ['X-Foo' => 'Bar'], $responseString),
+            new Response(400, ['content-type' => 'application/json'], $responseString400),
+            new Response(401, ['content-type' => 'application/json'], $responseString401)
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+
+        $config = RestConfig::getInstance("");
+        $routingObject = new RoutingV8($config);
+        $routingObject->setHandler($handlerStack);
+        $routingActions = $routingObject
+            ->startingPoint(52.5160, 13.3779)
+            ->destination(52.5185, 13.4283)
+            ->byCar()
+            ->getDefaultActions();
+
+        $this->assertIsArray($routingActions, "Routing: Basic GET car actions with mock");
+        $this->assertSame(2, sizeof($routingActions), "Routing: count actions with mock");
+
+        $routingActions = $routingObject
+            ->startingPoint(52.5160, 13.3779)
+            ->destination(52.5185, 13.4283)
+            ->byCar()
+            ->getDefaultActions();
+
+        $this->assertIsArray($routingActions, "Routing: Basic GET car actions with mock");
+        $this->assertSame(0, sizeof($routingActions), "Routing: count actions from error with mock");
 
     }
 
